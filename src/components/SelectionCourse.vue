06e253c940c85afab950a61d2047549abf47296c
mustafakba <kaba.mustafaa@gmail.com>
2022-11-17 01:19:02 +0300

    course card-container updated

--- src/components/SelectionCourse.vue
+++ src/components/SelectionCourse.vue
@@ -1,11 +1,20 @@
 <template>
-    <div class="container mx-auto mt-12 text-left items-center px-7">
+    <div class="container mx-auto mt-12 text-left items-center px-7 pb-32">
       <div class="comment-title ">
           <h2 class="title">Kapsamlı kurs seçkisi</h2>
           <div class="comment mt-3.5">Her ay yayınlanan yeni kurslarla 204.000 online video kurs arasından birini seçin</div>
       </div>
         <div class="lesson-list flex mt-5">
-           <div @click="getState" v-for="(lesson,index) in lessons" :key="index" class="lesson mr-6 pl-1.5">{{lesson}}</div>
+           <div @click="getState" v-for="(lesson,index) in lessons" :key="index" class="lesson mr-6 pl-1 hover:opacity-1 ">{{lesson}}</div>
+        </div>
+        <div class="card-container">
+            <div class="card-container-title my-2">{{this.$store.state.lesson_name.title}}</div>
+            <div style="max-width: 800px" class="card-container-paragraph my-2">
+                {{this.$store.state.lesson_name.paragraph}}
+            </div>
+                <button type="button" class="card-button-item mt-5 mb-3 font-bold button-width flex-nowrap text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" data-v-facba978="">
+                    {{ this.$store.state.lesson_name.button_title }}
+                </button>
         </div>
     </div>
 </template>
@@ -56,11 +65,39 @@ export default {
     line-height: 19.2px;
     text-align: center;
     color: #1c1d1f;
-    opacity: .7;
+    opacity: .9;
     transition: .3s;
 }
 .lesson:hover{
     opacity: 1;
-
+}
+.card-container{
+    border: 1px solid #D1D7DC;
+    padding:32px;
+    margin-top: 18px;
+}
+.card-container-title{
+    font-weight: 700;
+    font-size: 24px;
+    letter-spacing: -.2px;
+    height: 28.8px;
+    line-height: 28.8px;
+}
+.card-container-paragraph{
+    font-size: 16px;
+    line-height: 22.4px;
+    color: #1c1d1f;
+    height: 67.2px;
+    max-width: 800px;
+    overflow: hidden;
+}
+.card-button-item{
+    transition: .3s;
+    font-weight: 700;
+    color: #1c1d1f;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border:1px solid #1c1d1f;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -8,16 +8,16 @@ const store =createStore({
        return {
            lesson_name:
                {
-                   title:"",
-                   paragraph :"",
-                   button_title:"",
+                   title:"Python ile kariyer olanaklarınızı artırın",
+                   paragraph :"İster makine öğrenimi, ister finans alanında çalışıyor olun, ister web geliştirme veya veri bilimi alanında kariyer yapıyor olun, Python öğrenebileceğiniz en önemli becerilerden biridir. Python'un basit sözdizimi özellikle masaüstü, web ve iş uygulamaları için uygundur." ,
+                   button_title:"Python kurslarını keşfedin",
                    courses:[
                        {
-                           image_url:"",
-                           author:"",
-                           rate:"",
-                           price:"",
-                           sticker:"",
+                           image_url:"src/image/mustafa-murat-coskun-python.jpg",
+                           author:"Mustafa Murat",
+                           rate:"5 Yıldız",
+                           price:"429₺",
+                           sticker:"En çok satan",
                        }
                    ]
                },
