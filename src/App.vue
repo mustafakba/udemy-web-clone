c1fa61e943d8121f2d01565ed903bfa1cfc24a90
mustafakba <kaba.mustafaa@gmail.com>
2022-12-13 21:05:47 +0300

    App.vue moved to HomePage because we use router

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,13 @@
 <template>
-    <HeaderSection></HeaderSection>
-    <TopSlide></TopSlide>
-    <SelectionCourse></SelectionCourse>
-    <StudentsLook></StudentsLook>
-    <PopulerCategories></PopulerCategories>
-    <TrendingTopics></TrendingTopics>
-    <BecomeInstructor></BecomeInstructor>
-    <TrustingTeams></TrustingTeams>
-    <UdemyBusiness></UdemyBusiness>
-    <TheFooter></TheFooter>
+    <router-view></router-view>
 </template>
 
 <script>
-import HeaderSection from "@/components/Shared/HeaderSection";
-import TopSlide from "@/components/TopSlide";
-import SelectionCourse from "@/components/SelectionCourse";
-import StudentsLook from "@/components/StudentsLook";
-import PopulerCategories from "@/components/PopulerCategories";
-import TrendingTopics from "@/components/TrendingTopics"
-import BecomeInstructor from "@/components/BecomeInstructor";
-import TrustingTeams from "@/components/TrustingTeams";
-import UdemyBusiness from "@/components/UdemyBusiness";
-import TheFooter from "@/components/Shared/TheFooter";
+
 export default {
   name: "App",
   components: {
-      HeaderSection,
-      TheFooter,
-      UdemyBusiness,
-      TrustingTeams,
-      BecomeInstructor,
-      PopulerCategories,
-      StudentsLook,
-      SelectionCourse,
-      TopSlide,
-      TrendingTopics,
+
 
   },
 };
---
+++ src/Pages/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+    <HeaderSection></HeaderSection>
+    <TopSlide></TopSlide>
+    <SelectionCourse></SelectionCourse>
+    <StudentsLook></StudentsLook>
+    <PopulerCategories></PopulerCategories>
+    <TrendingTopics></TrendingTopics>
+    <BecomeInstructor></BecomeInstructor>
+    <TrustingTeams></TrustingTeams>
+    <UdemyBusiness></UdemyBusiness>
+    <TheFooter></TheFooter>
+</template>
+
+<script>
+import HeaderSection from "@/components/Shared/HeaderSection";
+import TopSlide from "@/components/TopSlide";
+import SelectionCourse from "@/components/SelectionCourse";
+import StudentsLook from "@/components/StudentsLook";
+import PopulerCategories from "@/components/PopulerCategories";
+import TrendingTopics from "@/components/TrendingTopics"
+import BecomeInstructor from "@/components/BecomeInstructor";
+import TrustingTeams from "@/components/TrustingTeams";
+import UdemyBusiness from "@/components/UdemyBusiness";
+import TheFooter from "@/components/Shared/TheFooter";
+export default {
+    name: "HomePage",
+    components: {
+        HeaderSection,
+        TheFooter,
+        UdemyBusiness,
+        TrustingTeams,
+        BecomeInstructor,
+        PopulerCategories,
+        StudentsLook,
+        SelectionCourse,
+        TopSlide,
+        TrendingTopics,
+
+    },
+};
+</script>
+
+<style>
+#app{
+}
+</style>
