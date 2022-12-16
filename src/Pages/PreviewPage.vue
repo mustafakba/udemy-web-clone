fc399ab9f7fbf81e580594d7a0865755e3b47111
mustafakba <kaba.mustafaa@gmail.com>
2022-12-17 00:39:55 +0300

    Preview Page course-detail container styles updated

--- src/Pages/PreviewPage.vue
+++ src/Pages/PreviewPage.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="container grid place-items-center ">
-        <div class="course-detail">
-            <div class="title">{{capitalized($route.params.name)}}</div>
-            <div class="course-inner">{{this.$store.state.lesson_name.title}}</div>
-            <div class="course-paragraph">{{this.$store.state.lesson_name.paragraph}}</div>
-            <div class="course-point flex items-center">
+    <div class="container2 grid grid-cols-2 place-content-evenly place-items-center py-10 px-10 ">
+        <div class="course-detail place-self-end ">
+            <div class="title blue-text">Yazılım > {{capitalized($route.params.name)}}</div>
+            <div class="course-title my-3">{{this.$store.state.lesson_name.title}}</div>
+            <div class="course-paragraph my-3">{{this.$store.state.lesson_name.paragraph}}</div>
+            <div class="course-point flex items-center my-3">
                 <span style="color: #FFC784">{{this.$store.state.lesson_name.courses[0].rate}}</span>
-                <span class="flex"><StarIcon v-for="i in 5" :key="i"></StarIcon></span>
+                <span class="flex ml-2 mr-2"><StarIcon v-for="i in 5" :key="i"></StarIcon></span>
                 <span>{{this.$store.state.lesson_name.courses[0].vote}}</span>
             </div>
+            <div class="course-author my-3">Oluşturan <span class="blue-text underline">{{this.$store.state.lesson_name.courses[0].author}}</span></div>
+            <div class="last-update-date flex items-center">
+                <span style="background: #F8F9FB ; width: 16px ;height: 16px;" class="rounded-full" ><img style="width: 16px ; " src="../../src/image/exclamation.svg" alt=""></span>
+                <span class="ml-2">Son güncelleme tarihi: 07/2020</span>
+                <span class="ml-2" style="background: #F8F9FB; border-radius: 50%">
+                    <img width="16px;" src="../image/language.svg" alt="">
+                </span>
+                <span class="ml-2">Türkçe</span>
+            </div>
+        </div>
+        <div class="course-preview">
+            vide gelecek buraya
         </div>
     </div>
     <button @click="goHome">Go To HomePage</button>
@@ -37,10 +49,25 @@ export default {
 </script>
 
 <style scoped>
-.container{
+.container2{
     color: #F8F9FB;
     background-color:#1C1D1F;
-    width: 100%;
 }
+.blue-text{
+    color: #CEC0FC;
+    cursor: pointer;
+}
+.course-title{
+    font-size: 32px;
+    font-weight: 700;
+    letter-spacing: -.2px;
+    line-height: 38.4px;
+    color: #Fff;
 
+}
+.course-paragraph{
+    font-size: 17px;
+    line-height: 26.6px;
+    color: #fff;
+}
 </style>
