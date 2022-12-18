c9dfed19974e1fd8c4b4e1cbe4e90b46381148cd
mustafakba <kaba.mustafaa@gmail.com>
2022-12-19 01:56:42 +0300

    PreviewContent component created

--- src/Pages/PreviewPage.vue
+++ src/Pages/PreviewPage.vue
@@ -1,8 +1,7 @@
 <template>
-          <div class="">
-                  <PreviewDetail ></PreviewDetail>
-                  <PreviewTeach class="mt-5"></PreviewTeach>
-              </div>
+    <PreviewDetail></PreviewDetail>
+    <PreviewTeach class="mt-5"></PreviewTeach>
+    <PreviewContent></PreviewContent>
 
 </template>
 
@@ -10,10 +9,12 @@
 
 import PreviewDetail from "@/components/PreviewDetail";
 import PreviewTeach from "@/components/PreviewTeach";
+import PreviewContent from "@/components/PreviewContent";
 
 export default {
     name: "PreviewPage",
     components: {
+        PreviewContent,
         PreviewTeach,
         PreviewDetail,
     }
@@ -21,10 +22,10 @@ export default {
 }
 </script>
 <style>
-    .container2{
-        min-width: 1390px;
-        max-width: 2100px;
-        color: #F8F9FB;
+.container2 {
+    min-width: 1390px;
+    max-width: 2100px;
+    color: #F8F9FB;
 
-    }
+}
 </style>
---
+++ src/components/PreviewContent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main-container grid grid-cols-3 px-60 mt-3 pb-10 ">
+        <div class="content col-span-2">
+            <h3 class="title">Kurs İçeriği</h3>
+            <div class="episodes-area flex justify-between mt-4">
+                <span>23 bölüm • 111 ders • 11 sa 3 dak toplam uzunluk</span>
+                <span style="color: rgba(64,27,156,0.8);font-size: 14px;font-weight: 700;" class="cursor-pointer episodes-expands">Tüm Bölümleri Genişlet</span>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PreviewContent"
+}
+</script>
+
+<style scoped>
+
+.title{
+    font-size: 24px;
+    font-weight: 700;
+}
+.episodes-expands:hover{
+    color: rgba(64,27,156,1)}
+</style>
