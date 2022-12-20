1a64e7dbf389db81992f6d65a018a6fc5267c777
mustafakba <kaba.mustafaa@gmail.com>
2022-12-20 22:40:11 +0300

    collapse extension added the project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import VueSplide from '@splidejs/vue-splide';
 import '@splidejs/vue-splide/css';
-
 import './assets/tailwind.css'
 import router from "../src/router.js";
 import store from "./store";
+import 'tw-elements';
+
+
+
 
 
 const app = createApp( App );
