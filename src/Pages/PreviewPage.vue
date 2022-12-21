fad15e3013701e979d251d9084e93766b1ef5172
mustafakba <kaba.mustafaa@gmail.com>
2022-12-21 22:25:56 +0300

    PreviewOtherCourse component created

--- src/Pages/PreviewPage.vue
+++ src/Pages/PreviewPage.vue
@@ -4,6 +4,7 @@
     <PreviewCompanies></PreviewCompanies>
     <PreviewContent></PreviewContent>
     <PreviewInformation></PreviewInformation>
+    <PreviewOthers></PreviewOthers>
 </template>
 
 <script>
@@ -13,10 +14,12 @@ import PreviewTeach from "@/components/PreviewTeach";
 import PreviewContent from "@/components/PreviewContent";
 import PreviewCompanies from "@/components/PreviewCompanies";
 import PreviewInformation from "@/components/PreviewInformation";
+import PreviewOthers from "@/components/PreviewOthers";
 
 export default {
     name: "PreviewPage",
     components: {
+        PreviewOthers,
         PreviewInformation,
         PreviewCompanies,
         PreviewContent,
---
+++ src/components/PreviewOthers.vue
@@ -0,0 +1,26 @@
+<template>
+ <div class="main-container grid grid-cols-3 px-60 mt-3 pb-10 ">
+     <div class="other-courses grid ">
+         <div class="title">Öğrencilerin kaydolduğu diğer kurslar </div>
+     </div>
+ </div>
+</template>
+
+<script>
+export default {
+    name: "PreviewOthers"
+}
+</script>
+
+<style scoped>
+.title{
+    margin-bottom: 20px;
+    font-size: 24px;
+    font-weight: 700;
+    letter-spacing: -0.2px;
+    line-height: 28.8px;
+    color: #1c1d1f;
+}
+
+
+</style>
