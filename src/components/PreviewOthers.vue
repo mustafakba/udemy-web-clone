ea363c15cd77f1874801443bfd96855b825cce1c
mustafakba <kaba.mustafaa@gmail.com>
2022-12-22 23:37:15 +0300

    PreviewOthers components completed

--- src/components/PreviewOthers.vue
+++ src/components/PreviewOthers.vue
@@ -1,48 +1,49 @@
 <template>
- <div class="main-container grid grid-cols-3 px-60 mt-3 pb-10 ">
-     <div class="other-courses grid col-span-2 ">
-         <div class="title">Öğrencilerin kaydolduğu diğer kurslar</div>
-         <div v-for="(course,index) in courseData" :key="index" class="course-container flex justify-evenly py-5">
-             <div class="course-content flex items-center justify-center text-left">
-                 <div :style="{ background: `url('${courseData[index].image}') no-repeat center` }" class="course-img w-20 h-20"></div>
-                 <div class="course-extansion">
-                    <div class="course-title">{{course.title}}</div>
-                    <div class="hours flex whitespace-nowrap">Toplam {{ course.total_hour }} saat  · <span class="text-gray-700! font-light" style="white-space: nowrap">Güncellendi {{ course.last_updated }} </span></div>
+    <div class="main-container grid grid-cols-3 px-60 mt-3 pb-10 ">
+        <div class="other-courses grid col-span-2 ">
+            <div class="title">Öğrencilerin kaydolduğu diğer kurslar</div>
+            <div v-for="(course,index) in courseData" :key="index" class="course-container flex justify-between py-2.5">
+                <div class="course-content flex items-center justify-center text-left">
+                    <div :style="{ background: `url('${courseData[index].image}') no-repeat center` }"
+                         class="course-img w-20 h-20"></div>
+                    <div class="course-extansion">
+                        <div class="course-title">{{ course.title }}</div>
+                        <div class="hours flex ">Toplam {{ course.total_hour }} saat ·
+                            <span class="ml-0.5 font-light" style="color: #6a6f73!important;">   Güncellendi {{course.last_updated }} </span></div>
+                    </div>
                 </div>
-             </div>
-             <div class="courses-values grid grid-cols-3 justify-center ml-5 ">
-                 <div style="height: 18px;" class="course-rate flex mt-1 justify-center ">
-                         <span class="flex items-center mr-1">{{course.rate}}</span>
-                         <StarIcon class="flex items-center"></StarIcon>
-                 </div>
-                 <div style="height: 18px;" class="course-members mt-1 flex justify-center ">
-                     <img  src="../image/users.svg" class="flex items-center mr-1" alt="">
-                     <span class="flex items-center">{{course.bought}}</span>
-                 </div>
-                 <div style="height: 18px;" class="course-price ml-5 h-4 ">
-                     <span class="h-4">{{course.price}}</span>
-                 </div>
-             </div>
-             <div class="heart-button flex justify-end ml-6">
-                 <span>Heart Button gelecek</span>
-             </div>
-         </div>
-     </div>
- </div>
+                <div style="height: 18px;" class="course-rate flex mt-1 ml-2 justify-center ">
+                    <span class="flex items-center mr-1">{{ course.rate }}</span>
+                    <StarIcon class="flex items-center"></StarIcon>
+                </div>
+                <div style="height: 18px;" class="course-members mt-1  ml-2 flex justify-end ">
+                    <img src="../image/users.svg" class="flex items-center mr-1" alt="">
+                    <span class="flex items-center">{{ course.bought }}</span>
+                </div>
+                <span class="h-4 font-bold">{{ course.price }}</span>
+                <div class="heart-button">
+                        <div class="p-3 border heart-button-container">
+                            <img class="heart-svg" src="../image/heart.svg" alt="">
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import courseData from '../apis/courses.json'
 import StarIcon from "@/Icons/starIcon";
+
 export default {
     name: "PreviewOthers",
     components: {StarIcon},
-    data(){
-        return{
-            courseData : courseData,
+    data() {
+        return {
+            courseData: courseData,
         }
     },
-    methods(){
+    methods() {
 
     },
 
@@ -50,14 +51,16 @@ export default {
 </script>
 
 <style scoped>
-.course-img{
+.course-img {
     margin-right: 15px;
     width: 80px;
 }
-.course-title{
+
+.course-title {
     font-weight: 700;
 }
-.title{
+
+.title {
     margin-bottom: 20px;
     font-size: 24px;
     font-weight: 700;
@@ -65,12 +68,30 @@ export default {
     line-height: 28.8px;
     color: #1c1d1f;
 }
-.hours{
+.heart-button-container{
+    cursor: pointer;
+    border: 1px solid #1c1d1f;
+    border-radius: 50%;
+    transition: .2s;
+}
+.heart-button-container:hover{
+    background-color: rgba(106, 111, 115, 0.1);
+}
+.heart-svg{
+    width: 25px;
+    height: 25px;
+}
+.hours {
     color: #1E6055;
     font-weight: 700;
     font-size: 16px;
 }
-.course-container{
+
+.course-content {
+    max-width: 450px;
+}
+
+.course-container {
     border-bottom: 1px solid #6a6f73;
 }
 
